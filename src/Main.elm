90488e5879828f1f9037f6e402ea6cd3d9193f43
Ben Attenborough <ben.attenborough@featurespace.co.uk>
2022-09-13 16:39:16 +0100

    WIP Work on HTTP request

--- src/Main.elm
+++ src/Main.elm
@@ -5,40 +5,68 @@ import Html exposing (Html, button, input, div, text)
 import Html.Events exposing (onClick)
 import Html.Attributes exposing (placeholder, value)
 import Html.Events exposing (onInput)
-
+import Http
+import Json.Decode exposing (Decoder, map4, field, int, string)
 
 type alias Model =
-    { postCode : String }
+    { postCode : String
+    , isLoading : Bool }
 
 
-initialModel : Model
-initialModel =
-    { postCode = "" }
+initialModel : () -> (Model, Cmd Msg)
+initialModel _ =
+    ({ postCode = "" 
+    , isLoading = True
+    },
+    Cmd.none)
+
+subscriptions : Model -> Sub Msg
+subscriptions _ =
+  Sub.none
 
 
 type Msg
-    = Submit String
+    = OnChange String
+    | Submit
+    | Loading
+    -- | GotPostcode (Result Http.Error Response)
 
 
-update : Msg -> Model -> Model
+update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
     case msg of
-        Submit code ->
-            { model | postCode = code }
+        OnChange code ->
+            ({ model | postCode = code }, Cmd.none)
+        
+        Submit ->
+            (model, Cmd.none)
+        
+        Loading ->
+            (model, Cmd.none)
 
+        -- GotPostcode ->
+        --     (model, Cmd.none)
 
 view : Model -> Html Msg
-view model =
+view ({postCode, isLoading})  =
     div []
-        [ input [ placeholder "Postcode", value model.postCode, onInput Submit ] []
-        , div [] [text model.postCode]
+        [ input [ placeholder "Postcode", value postCode, onInput OnChange ] []
+        , button [ onClick Submit ] [ text "Submit" ]
+        , div [] [text postCode]
+        , div [] [
+            if isLoading == True then
+                text "Loading data"
+            else
+                text "Waiting for input"
+        ]
         ]
 
 
 main : Program () Model Msg
 main =
-    Browser.sandbox
+    Browser.element
         { init = initialModel
+        , subscriptions = subscriptions
         , view = view
         , update = update
         }
